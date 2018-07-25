3485c0d8d896202aaec229219e71aa30314efb86
iatanasov <i.atanasov@is-bg.net>
2018-07-25 12:07:21 +0300

    fix abstract error

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-     public abstract  class StandardGradeBook :BaseGradeBook
+     public  class StandardGradeBook :BaseGradeBook
     {
         public StandardGradeBook(string name):base(name)
         {
